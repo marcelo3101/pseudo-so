8e3eea91afc2e08b3c5854105f5bc4f52579f99e
map3101 <marcelo.postiglione@gmail.com>
2023-01-16 11:02:52 -0300

    Melhor construtor para a classe File

--- file.py
+++ file.py
@@ -2,8 +2,13 @@
 # DEVE ter um campo com o processo que a criou
 
 class File:
-    def __init__(self, name, first_block, memory_blocks, process_id = None) -> None:
-        self.name = name  # Nome do arquivo
-        self.first_block = first_block  # Primeiro bloco que ocupa no disco
-        self.memory_blocks = memory_blocks  # Quantidade de blocos do disco que ocupa
+    def __init__(self, input, process_id = None) -> None:
+        # Tratar input
+        input = input.replace("\n", "")
+        input = input.replace(",", "")
+        input = input.split(" ")
+
+        self.name = input[0]  # Nome do arquivo
+        self.first_block = int(input[1])  # Primeiro bloco que ocupa no disco
+        self.memory_blocks = int(input[2])  # Quantidade de blocos do disco que ocupa
         self.process_id = process_id  # ID do processo que criou, caso tiver
