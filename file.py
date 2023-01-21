51f32d833fb08261eae4a6b9d795745e17c5989d
Antônio Vinicius <avmoura.r@gmail.com>
2023-01-21 01:59:49 -0300

    :sparkles: start: file_manager struct

--- file.py
+++ file.py
@@ -1,2 +1,15 @@
 # Classe que representa o arquivo
 # DEVE ter um campo com o processo que a criou
+
+class File:
+    def __init__(self, file, creator = None):
+        file = file.split(",")
+
+        self.name = file[0]
+        self.start_block = int(file[1])
+        self.size = int(file[2])
+        self.creator = creator
+
+
+# file = File("file_name, 5, 3", "antonio vinicius")
+# print(file.name)
