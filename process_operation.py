df484c12f235d6cac50cea60c7d85c73dbf2ba62
map3101 <marcelo.postiglione@gmail.com>
2023-01-30 18:57:18 -0300

    Corrige __init__

--- process_operation.py
+++ process_operation.py
@@ -1,9 +1,12 @@
 class ProcessOperation:
-    def __init__(self, operation) -> None:
-        operation = operation.split(',')
-        self.pid = int(operation[0])
-        self.opcode = int(operation[1])
-        self.filename = operation[2]
+    def __init__(self, input) -> None:
+        input = input.replace("\n", "")
+        input = input.replace(" ", "")
+        input = input.split(",")
+
+        self.pid = int(input[0])
+        self.opcode = int(input[1])
+        self.filename = input[2]
         # Checar se for operação de criar arquivo e definir valor para o tamanho
-        if self.opcode == 0: self.file_size = int(operation[3])
+        if self.opcode == 0: self.file_size = int(input[3])
         else: self.file_size = None
